5a85145c425e990a4f4fa3221f092133178f002e
GeorgeBish93 <George.bisharat93@gmail.com>
2023-03-18 17:46:28 +0200

    simple ts and css

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
-  },
+  baseURL: "https://api.escuelajs.co/api/v1",
 });

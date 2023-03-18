69556817a86bf064d73ac2cbcff6e8ecbdfd1356
GeorgeBish93 <George.bisharat93@gmail.com>
2023-03-18 15:47:58 +0200

    Create Search input + chakra css

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -9,6 +9,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import useCategory, { Category } from "../hooks/useCategory";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectCategory: (category: Category) => void;
@@ -21,25 +22,34 @@ const CategoryList = ({ selectedCategory, onSelectCategory }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {data.map((category) => (
-        <ListItem key={category.id} paddingY="5px">
-          <HStack>
-            <Image boxSize="20px" borderRadius={8} src={category.image}></Image>
-            <Button
-              fontWeight={
-                category.id === selectedCategory?.id ? "bold" : "normal"
-              }
-              onClick={() => onSelectCategory(category)}
-              fontSize="lg"
-              variant="link"
-            >
-              {category.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginTop={9} marginBottom={3}>
+        Categories
+      </Heading>
+      <List>
+        {data.map((category) => (
+          <ListItem key={category.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="30px"
+                borderRadius={8}
+                src={getCroppedImageUrl(category.image)}
+              ></Image>
+              <Button
+                fontWeight={
+                  category.id === selectedCategory?.id ? "bold" : "normal"
+                }
+                onClick={() => onSelectCategory(category)}
+                fontSize="lg"
+                variant="link"
+              >
+                {category.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 

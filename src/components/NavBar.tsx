03728c47506539782a15db4d4b61c200f4fe65fd
GeorgeBish93 <George.bisharat93@gmail.com>
2023-03-19 14:28:13 +0200

    change navbar icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/plogo.png";
+import logo from "../assets/bLogo.png";
+import CategoryHeading from "./CategoryHeading";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
